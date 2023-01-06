7093aa3814bd1d6af24de57945279ebeb834e462
Chris Bingham <binghamchris@users.noreply.github.com>
2023-01-06 13:32:01 +0100

    Expanded URL Sharing
    Expanded the basic URL sharing example with multiple URLs, selectable via the buttons on the device.

--- code.py
+++ code.py
@@ -3,21 +3,30 @@ from demo_badge.expresslink import Event, OTACodes
 
 badge = Badge()
 
+def change_url(url_to_share):
+    print(f"URL to share is: {url_to_share}")
+
+    print("Setting NFC tag...")
+    badge.nfc_tag.set_url(url_to_share)
+
+    print("Displaying QR code...")
+    badge.show_qr_code(url_to_share)
+
 # Connect to AWS, stop if there is any error
 success, status, err = badge.expresslink.connect()
 if not success:
     print(f"Unable to connect: {err} {status}")
     while True: pass
 
-url_to_share = "https://cloudypandas.ch"
-
-print(f"URL to share is: {url_to_share}")
-
-print("Setting NFC tag...")
-badge.nfc_tag.set_url(url_to_share)
-
-print("Displaying QR code...")
-badge.show_qr_code(url_to_share)
+change_url("https://cloudypandas.ch")
 
 while True:
     badge.update()
+
+    if badge.button1.pressed:
+        change_url("https://cloudypandas.ch")
+    if badge.button2.pressed:
+        change_url("https://www.cloudreach.com/en/technical-blog/")
+    if badge.button3.pressed:
+        change_url("https://github.com/binghamchris")
+    
